dadab88b554acf4c1212aff5897d72af86f2e5fb
Tunghsiao Liu <info@sparanoid.com>
2012-10-14 15:50:26 +0800

    Load child theme stylesheet in a more graceful way to avoid CSS @import. More information about CSS @import: http://www.stevesouders.com/blog/2009/04/09/dont-use-import/

--- functions.php
+++ functions.php
@@ -1,11 +1,34 @@
 <?php
 /**
  * Twenty Twelve Kai functions and definitions
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve_Kai
+ * @since Twenty Twelve Kai 1.0
  */
 
+/**
+ * Customize footer text
+ *
+ * @since Twenty Twelve Kai 1.0
+ */
 function add_twentytwelve_credits() {
   echo 'Theme: <a href="http://postholic.com/rsrc/twentytwelve-kai/">Twenty Twelve Kai</a><span class="sep">.</span> ';
 }
 
 add_action('twentytwelve_credits', 'add_twentytwelve_credits');
+
+/**s
+ * Override Twenty Twelve stylesheet loading behavier and load Twenty Twelve Kai stylesheet
+ *
+ * @since Twenty Twelve Kai 1.1
+ */
+function twentytwelve_scripts_styles_override() {
+ 	/*
+ 	 * Loads our main stylesheet.
+ 	 */
+   wp_enqueue_style( 'twentytwelve-style', get_template_directory_uri() . '/style.css' );
+   wp_enqueue_style( 'twentytwelve-kai-style', get_stylesheet_directory_uri() . '/core.css' );
+ }
+add_action( 'wp_enqueue_scripts', 'twentytwelve_scripts_styles_override' );
 ?>

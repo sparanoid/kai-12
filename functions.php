021926c1a657a8f40d0a3664366932e320452d01
Tunghsiao Liu <info@sparanoid.com>
2012-10-17 20:44:05 +0800

    Add theme auto updater support

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@ function add_twentytwelve_credits() {
 
 add_action('twentytwelve_credits', 'add_twentytwelve_credits');
 
-/**s
+/**
  * Override Twenty Twelve stylesheet loading behavier and load Twenty Twelve Kai stylesheet
  *
  * @since Twenty Twelve Kai 1.1
@@ -27,5 +27,17 @@ function twentytwelve_scripts_styles_override() {
    wp_enqueue_style( 'twentytwelve-style', get_template_directory_uri() . '/style.css' );
    wp_enqueue_style( 'twentytwelve-kai-style', get_stylesheet_directory_uri() . '/core.css' );
  }
+
 add_action( 'wp_enqueue_scripts', 'twentytwelve_scripts_styles_override' );
+
+/**
+ * Initialize the update checker
+ *
+ * @since Twenty Twelve Kai 1.1
+ */
+require 'includes/theme-update-checker.php';
+$twentytwelve_kai_update_checker = new ThemeUpdateChecker(
+  'twentytwelve-kai',
+  'http://sparanoid.com/lab/wordpress/twentytwelve-kai.json'
+);
 ?>

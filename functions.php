f5b3e52ced0892c1521a2f5a14098221519f044e
sparanoid <info@sparanoid.com>
2012-11-05 10:46:32 +0800

    Update bundled Infinite Scroll support for default themes.
    Specifically, in Twenty Twelve, check for sidebar widgets on mobile devices since sidebars move below post list.

--- functions.php
+++ functions.php
@@ -60,8 +60,28 @@ $twentytwelve_kai_update_checker = new ThemeUpdateChecker(
 function twenty_twelve_kai_infinite_scroll_init() {
   add_theme_support( 'infinite-scroll', array(
     'container'      => 'content',
-    'footer_widgets' => false
+    'footer'         => 'page'
 ) );
 }
 add_action( 'after_setup_theme', 'twenty_twelve_kai_infinite_scroll_init' );
+
+/**
+ * Handle `footer_widgets` argument for mobile devices
+ *
+ * @param bool $has_widgets
+ * @uses jetpack_is_mobile, is_front_page, is_active_sidebar
+ * @filter infinite_scroll_has_footer_widgets
+ * @return bool
+ */
+function twenty_twelve_kai_has_footer_widgets( $has_widgets ) {
+  if ( function_exists( 'jetpack_is_mobile' ) && jetpack_is_mobile() ) {
+    if ( is_front_page() && ( is_active_sidebar( 'sidebar-2' ) || is_active_sidebar( 'sidebar-3' ) ) )
+      $has_widgets = true;
+    elseif ( is_active_sidebar( 'sidebar-1' ) )
+      $has_widgets = true;
+  }
+
+  return $has_widgets;
+}
+add_filter( 'infinite_scroll_has_footer_widgets', 'twenty_twelve_kai_has_footer_widgets' );
 ?>

c4a193135700acf9bef9d27f6987442ec0ae0d08
Tunghsiao Liu <info@sparanoid.com>
2012-12-13 16:04:15 +0800

    Override default Jetpack Infinite Scroll footer

--- functions.php
+++ functions.php
@@ -13,7 +13,7 @@
  * @since Twenty Twelve Kai 1.0
  */
 function add_twentytwelve_credits() {
-  echo 'Theme: <a href="http://postholic.com/rsrc/twentytwelve-kai/">Twenty Twelve Kai</a><span class="sep">.</span> ';
+  echo '<a href="http://postholic.com/rsrc/twentytwelve-kai/">Twenty Twelve Kai</a><span class="sep"> - </span> ';
 }
 
 add_action('twentytwelve_credits', 'add_twentytwelve_credits');
@@ -24,9 +24,9 @@ add_action('twentytwelve_credits', 'add_twentytwelve_credits');
  * @since Twenty Twelve Kai 1.1
  */
 function twentytwelve_scripts_styles_override() {
-   wp_enqueue_style( 'twentytwelve-style', get_template_directory_uri() . '/style.css' );
-   wp_enqueue_style( 'twentytwelve-kai-style', get_stylesheet_directory_uri() . '/core.css' );
- }
+  wp_enqueue_style( 'twentytwelve-style', get_template_directory_uri() . '/style.css' );
+  wp_enqueue_style( 'twentytwelve-kai-style', get_stylesheet_directory_uri() . '/core.css' );
+}
 
 add_action( 'wp_enqueue_scripts', 'twentytwelve_scripts_styles_override' );
 
@@ -40,6 +40,17 @@ function twentytwelve_kai_custom_excerpt_length( $length ) {
 }
 
 add_filter( 'excerpt_length', 'twentytwelve_kai_custom_excerpt_length', 999 );
+
+/**
+ * Override default Jetpack Infinite Scroll footer
+ *
+ * @since Twenty Twelve Kai 1.1.5
+ */
+function twentytwelve_kai_infinite_scroll_credit() {
+  return '<a href="http://postholic.com/rsrc/twentytwelve-kai/">Twenty Twelve Kai</a>';
+}
+
+add_filter( 'infinite_scroll_credit', 'twentytwelve_kai_infinite_scroll_credit' );
 
 /**
  * Initialize the update checker

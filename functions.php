47da749e33f0b169a156c6a0188de0596c9f0c7c
sparanoid <info@sparanoid.com>
2013-01-16 07:18:18 +0800

    Update screenshot, change theme name

--- functions.php
+++ functions.php
@@ -1,27 +1,27 @@
 <?php
 /**
- * Twenty Twelve Kai functions and definitions
+ * Kai 12 functions and definitions
  *
  * @package WordPress
- * @subpackage Twenty_Twelve_Kai
- * @since Twenty Twelve Kai 1.0
+ * @subpackage Kai_12
+ * @since Kai 12 1.0
  */
 
 /**
  * Customize footer text
  *
- * @since Twenty Twelve Kai 1.0
+ * @since Kai 12 1.0
  */
 function add_twentytwelve_credits() {
-  echo '<a href="http://postholic.com/rsrc/twentytwelve-kai/">Twenty Twelve Kai</a><span class="sep"> - </span> ';
+  echo '<a href="http://sparanoid.com/work/kai/">Kai 12</a><span class="sep"> - </span> ';
 }
 
 add_action('twentytwelve_credits', 'add_twentytwelve_credits');
 
 /**
- * Override Twenty Twelve stylesheet loading behavier and load Twenty Twelve Kai stylesheet
+ * Override Twenty Twelve stylesheet loading behavier and load Kai 12 stylesheet
  *
- * @since Twenty Twelve Kai 1.1
+ * @since Kai 12 1.1
  */
 function twentytwelve_scripts_styles_override() {
   wp_enqueue_style( 'twentytwelve-style', get_template_directory_uri() . '/style.css' );
@@ -33,7 +33,7 @@ add_action( 'wp_enqueue_scripts', 'twentytwelve_scripts_styles_override' );
 /**
  * Override default excerpt length
  *
- * @since Twenty Twelve Kai 1.1.2
+ * @since Kai 12 1.1.2
  */
 function twentytwelve_kai_custom_excerpt_length( $length ) {
   return 360;
@@ -44,10 +44,10 @@ add_filter( 'excerpt_length', 'twentytwelve_kai_custom_excerpt_length', 999 );
 /**
  * Override default Jetpack Infinite Scroll footer
  *
- * @since Twenty Twelve Kai 1.1.6
+ * @since Kai 12 1.1.6
  */
 function twentytwelve_kai_infinite_scroll_credit() {
-  return '<a href="http://postholic.com/rsrc/twentytwelve-kai/">Twenty Twelve Kai</a>';
+  return '<a href="http://sparanoid.com/work/kai/">Kai 12</a>';
 }
 
 add_filter( 'infinite_scroll_credit', 'twentytwelve_kai_infinite_scroll_credit' );

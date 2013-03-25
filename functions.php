1f870d47cfb6bf5723696d571126404e98ae99d2
Tunghsiao Liu <t@sparanoid.com>
2013-03-25 10:27:09 +0800

    * add_twentytwelve_credits -> add_kai_12_credits

--- functions.php
+++ functions.php
@@ -12,11 +12,11 @@
  *
  * @since Kai 12 1.0
  */
-function add_twentytwelve_credits() {
+function add_kai_12_credits() {
   echo '<a href="http://sparanoid.com/work/kai/">Kai 12</a><span class="sep"> - </span> ';
 }
 
-add_action('twentytwelve_credits', 'add_twentytwelve_credits');
+add_action('twentytwelve_credits', 'add_kai_12_credits');
 
 /**
  * Override Twenty Twelve stylesheet loading behavier and load Kai 12 stylesheet
@@ -25,7 +25,7 @@ add_action('twentytwelve_credits', 'add_twentytwelve_credits');
  */
 function twentytwelve_scripts_styles_override() {
   wp_enqueue_style( 'twentytwelve-style', get_template_directory_uri() . '/style.css' );
-  wp_enqueue_style( 'twentytwelve-kai-style', get_stylesheet_directory_uri() . '/core.css' );
+  wp_enqueue_style( 'kai-12-style', get_stylesheet_directory_uri() . '/core.css' );
 }
 
 add_action( 'wp_enqueue_scripts', 'twentytwelve_scripts_styles_override' );

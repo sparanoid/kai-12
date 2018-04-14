2a4f479bd0aa230d96bcf3b19ddcfff1beb63f4d
Tunghsiao Liu <t@sparanoid.com>
2018-04-14 11:02:15 +0800

    fix(style): missing code typography on iOS, release 1.2.12

--- functions.php
+++ functions.php
@@ -89,7 +89,7 @@ function kai_12_header_style() {
 /**
  * Initialize the Infinite Scroll for Jetpack
  *
- * @since Kai 12 1.2.11
+ * @since Kai 12 1.2.12
  */
 function kai_12_infinite_scroll_init() {
   add_theme_support( 'infinite-scroll', array(
